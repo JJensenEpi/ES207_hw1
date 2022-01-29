3ba1700e3003c25f4c160c613aebb1f64faed49e
Jordan <98067683+JJensenEpi@users.noreply.github.com>
2022-01-29 15:33:49 -0800

    UPdated

--- datapull.R
+++ datapull.R
@@ -1,5 +1,5 @@
 #Pulls data from football-data.org
-
+##Created dashee87 as part of the footballR package 
 fdo_call_API <- function(request, token="", response = "full"){
   url <- paste0("http://api.football-data.org/",request)
   output <- httr::GET(url,httr::add_headers(`X-Auth-Token` = token, `X-Response-Control` = response))
