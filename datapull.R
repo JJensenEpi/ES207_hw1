37efe5a2eec63778720ce9d72db0f03145e48b4c
Jordan <98067683+JJensenEpi@users.noreply.github.com>
2022-01-29 15:32:25 -0800

    function for data pull

---
+++ datapull.R
@@ -0,0 +1,7 @@
+#Pulls data from football-data.org
+
+fdo_call_API <- function(request, token="", response = "full"){
+  url <- paste0("http://api.football-data.org/",request)
+  output <- httr::GET(url,httr::add_headers(`X-Auth-Token` = token, `X-Response-Control` = response))
+  return(jsonlite::fromJSON(httr::content(output, as = "text", encoding = "UTF-8")))
+}
